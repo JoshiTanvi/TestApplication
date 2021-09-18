4f5d9da19a3910d01c2920cf04a2fc1b56be664d
joshitanvi <tanvijoshi04@gmail.com>
2021-09-18 13:28:55 +0530

    Validations are added for mobile and email using regex.

--- app/src/main/java/com/example/testapplication/RegisterActivity.java
+++ app/src/main/java/com/example/testapplication/RegisterActivity.java
@@ -3,6 +3,10 @@ package com.example.testapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +14,33 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Button regibtn = findViewById(R.id.registerbtn);
+        EditText name = findViewById(R.id.name);
+        EditText mobile = findViewById(R.id.mobile);
+        EditText email = findViewById(R.id.email);
+        EditText pwd = findViewById(R.id.pwd);
+
+        regibtn.setOnClickListener(view -> {
+            if(name.getText().toString().isEmpty() || mobile.getText().toString().isEmpty() ||
+                    email.getText().toString().isEmpty()|| pwd.getText().toString().isEmpty()){
+                Toast.makeText(this,"All fields are required! Please fill the details",Toast.LENGTH_SHORT).show();
+
+
+            }else if(!mobile.getText().toString().matches("\\d{10}")){
+                Toast.makeText(this,"Please enter 10 digits indian mobile number.",Toast.LENGTH_SHORT).show();
+
+            }else if(!email.getText().toString().matches("^(.+){4,25}@(.+){4,25}$")){
+                Toast.makeText(this,"Please enter valid email.",Toast.LENGTH_SHORT).show();
+
+            }
+            else {
+                Toast.makeText(this,"Registration Successfully!",Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
+
+
+
 }
